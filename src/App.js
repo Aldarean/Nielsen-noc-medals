c1fabe252232e04a50105596c8ea7b95a96dd17a
Rodney Wormsbecher <RWormsbecher@ilionx.com>
2019-06-14 00:26:41 +0200

    installed packages and set app file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
-
 import "./App.css";
 
+import MedalData from "./data/medal-data.json";
+import CountryListComponent from "./components/CountryListComponent";
+
 function App() {
-	return <div className="App" />;
+	// sets the data for the medailes of a country.
+	// TODO: if multiple country data sets are present, use a Map function around this.
+	const medalCount = MedalData.NOCMedals.Medals;
+	const noc = MedalData.NOCMedals.NOC;
+	const sportslist = MedalData.SportList;
+
+	return (
+		<section className="medal-container">
+			<p>Click on one of the countries to see the details.</p>
+
+			<div className="country-container">
+				<CountryListComponent countryDetails={noc} sportslist={sportslist} />
+			</div>
+		</section>
+	);
 }
 
 export default App;
